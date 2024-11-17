458b0dc1cfb1be8d168e3a30434c6db8b4a79699
Seongjin Kang <65354945+enKODING1@users.noreply.github.com>
2024-11-17 17:24:27 +0900

    feat: basic blog structure

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,23 @@
+import { getAllPostIds, getPostData } from "../../../../lib/posts";
+
+export async function generateStaticParams() {
+  const posts = getAllPostIds();
+  return posts.map((post) => ({ slug: post }));
+}
+
+export default async function PostPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const id = (await params).slug;
+  const { title, date, content } = await getPostData(id);
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{date}</p>
+      <main>{content}</main>
+    </div>
+  );
+}
