9a696e81a208809d56ffe94d09a531275b9afa1d
Seongjin Kang <65354945+enKODING1@users.noreply.github.com>
2024-11-17 17:23:55 +0900

    feat: markdown parsing util

---
+++ lib/posts.ts
@@ -0,0 +1,65 @@
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export type PostData = {
+  id: string;
+  content: string;
+  date?: string;
+  title?: string;
+};
+
+export function getSortedPostsData(): PostData[] {
+  // Get file names under /posts
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName): PostData => {
+    // Remove ".md" from file name to get id
+    const id = fileName.replace(/\.md$/, "");
+
+    // Read markdown file as string
+    const fullPath = path.join(postsDirectory, fileName);
+    const fileContents = fs.readFileSync(fullPath, "utf8");
+
+    // Use gray-matter to parse the post metadata section
+    const { content, data } = matter(fileContents);
+
+    // Combine the data with the id
+    return {
+      id,
+      content,
+      ...data,
+    };
+  });
+  // Sort posts by date
+  return allPostsData.sort((a, b) => {
+    if (a.date && b.date && a.date < b.date) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+}
+
+export async function getPostData(id: string): Promise<PostData> {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const contents = fs.readFileSync(fullPath, "utf-8");
+  const { content, data } = matter(contents);
+
+  return {
+    id,
+    content,
+    ...data,
+  };
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
+    const id = fileName.replace(/\.md$/, "");
+    return id;
+  });
+  console.log(allPostsData);
+  return allPostsData;
+}
