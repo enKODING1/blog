ef7e4cd927a4e7294be80dd56d0bccd4592bb511
Seongjin Kang <65354945+enKODING1@users.noreply.github.com>
2024-12-10 23:03:52 +0900

    fix: display comment

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -3,19 +3,27 @@
 import { useEffect, useRef } from "react";
 
 export default function Comments() {
-  const ref = useRef(null);
+  const commentsRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    const script = document.createElement("script");
+    if (!commentsRef.current) {
+      console.error("Comments container not mounted yet");
+      return;
+    }
 
+    commentsRef.current.innerHTML = "";
+
+    const script = document.createElement("script");
     script.src = "https://utteranc.es/client.js";
     script.async = true;
     script.setAttribute("repo", "enkoding1/enkoding1.github.io");
-    script.setAttribute("issue-term", "title");
+    script.setAttribute("issue-term", "pathname");
     script.setAttribute("label", "comments");
     script.setAttribute("theme", "github-light");
     script.setAttribute("crossorigin", "anonymous");
+
+    commentsRef.current.appendChild(script);
   }, []);
 
-  return <div ref={ref}></div>;
+  return <div ref={commentsRef}></div>;
 }
