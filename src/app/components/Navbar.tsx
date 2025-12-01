8456b96e9bd45a4e73f0908ecd2e69a729bc6c1d
Seongjin Kang <65354945+enKODING1@users.noreply.github.com>
2025-12-01 17:46:35 +0900

    feat: Remove custom page transition logic and replace `TransitionLink` with `next/link` components.

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,11 +1,11 @@
-import TransitionLink from "./TransitionLink";
+import Link from "next/link";
 
 export default function Navbar() {
   return (
     <nav className="w-full p-4 fixed top-0 left-0 backdrop-blur-[8px] backdrop-saturate-[100%] bg-opacity-50">
-      <TransitionLink href="/">
+      <Link href="/">
         <h1 className="text-xl hover:underline">ENKODING BLOG</h1>
-      </TransitionLink>
+      </Link>
     </nav>
   );
 }
--- src/app/template.tsx
+++ src/app/template.tsx
@@ -1,17 +1,3 @@
-"use client";
-
-import { useEffect } from "react";
-import { animatePageIn } from "./utils/BlindAnimation";
-import BlindTransition from "./components/Transition/BlindTransition";
-
 export default function Template({ children }: { children: React.ReactNode }) {
-  useEffect(() => {
-    animatePageIn();
-  }, []);
-
-  return (
-    <>
-      <BlindTransition>{children}</BlindTransition>
-    </>
-  );
+  return <>{children}</>;
 }
