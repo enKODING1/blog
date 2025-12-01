b3123bd52db0f2447554bf139853639f26e37d05
Seongjin Kang <65354945+enKODING1@users.noreply.github.com>
2025-12-01 20:19:08 +0900

    style: customize typography for code blocks in Tailwind CSS.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,25 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            "code::before": {
+              content: '""',
+            },
+            "code::after": {
+              content: '""',
+            },
+            code: {
+              backgroundColor: "rgba(135,131,120,0.15)",
+              color: "#EB5757",
+              padding: "0.2em 0.4em",
+              borderRadius: "3px",
+              fontWeight: "500",
+            },
+          },
+        },
+      },
     },
   },
   plugins: [typography],
